dfc96a65f0b2e5df791fba889eda649cc629cf72
alvaroxdzy <alvaroxdzy3@gmail.com>
2022-08-26 11:25:44 -0400

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,25 +3,6 @@
 @section('content')
 <div class="container">
     <img id="fondo_home" src="{{ asset('/img/Fondo.jpg')}} " width="1275px" height="600px"  />
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">
-                    
-                </div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
 </div>
 
 
