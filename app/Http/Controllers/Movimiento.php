195e4f7743d97913cab438bf7b7b2845385cc9aa
alvaroxdzy <alvaroxdzy3@gmail.com>
2022-09-02 11:47:13 -0400

    todo funcionando,pruebas con movimiento y el table

--- app/Http/Controllers/Movimiento.php
+++ app/Http/Controllers/Movimiento.php
@@ -3,14 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Proveedor;
+use App\Models\Bodega;
+use DB;
+
+
 
 class Movimiento extends Controller
 {
         public function create()
     {
       // $region = Regions::select('id','name')->get();
-      // $comuna = Communes::select('id','name')->get();
+         $bodega = Bodega::select('codigo_bodega','nombre_bodega')->get();
+         $proveedor= Proveedor::select(DB::raw("CONCAT(rut_proveedor,'-',dig_rut_prov)as rut_proveedor"),'razon_social')->orderBy('razon_social')->get();
 
-        return view('movimientoIngreso');
+
+        return view('movimientoIngreso')->with('proveedor',$proveedor)->with('bodega',$bodega);
     }
 }
--- resources/views/movimientoIngreso.blade.php
+++ resources/views/movimientoIngreso.blade.php
@@ -5,54 +5,60 @@
 <div class="card"> 
     <div class="card-body">
         <div>
-            <div class="row"> 
+           <div class="row">
+            <div class="mb-3 col-md-2">
+                <label> TIPO DE DOCUMENTO</label>
+                <select class="form-control"> 
+                    <option>FACTURA </option>
+                    <option>GUIA </option>
+                    <option>COMPROBANTE DE INGRESO </option>
+                </select>
+            </div>
+            <div class="mb-3 col-md-2">
+                <label> NRO DOCUMENTO </label>
+                <input  class="form-control" type="text" name="">
+            </div>
+            <div class="mb-3 col-md-2">
+                <label> PROVEEDOR  </label>
+                <select class="form-control">
+                    <option value="">----- NO POSEE -----</option>
+                    @foreach($proveedor as $proveedores)
+                    <option value="{{$proveedores->rut_proveedor}}"> {{$proveedores->razon_social}} </option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+        <div class="row"> 
+            <div class="mb-3 col-md-2">
+                <label> BODEGA  </label>
+                <select class="form-control">
 
-                <div class="mb-3 col-md-2">
-                    <label> N° MOVIMIENTO</label>
-                    <input class="form-control "name="" type="text">
-                </div>
-
-                <div class="mb-3 col-md-2">
-                    <label> BODEGA  </label>
-                    <input class="form-control "name="" type="text">
-
-                </div>
-                <div class="mb-3 col-md-2">
-                    <label> FECHA  </label>
-                    <input class="form-control" name="fecha" type="text" size= "10"  id="fecha" value="<?php echo date("d/m/Y"); ?>" /> 
-
-                </div> 
-                <div class="mb-3 col-md-2">
-                    <label> TIPO DE MOVIMIENTO  </label>
-                    <input class="form-control "name="" type="text" value="INGRESO" disabled> 
-                </div>               
-                <div class="mb-3 col-md-2">
-                    <label> RUT PROVEEDOR  </label>
-                    <input class="form-control "name="" type="text" value="" > 
-                </div>
-
-                <div class="mb-3 col-md-2">
-                    <label> Estado  </label>
-                    <input class="form-control "name="" type="" value="DISPONIBLE" disabled > 
-                </div>
+                    @foreach($bodega as $bodegas)
+                    <option value="{{$bodegas->codigo_bodega}}"> {{$bodegas->nombre_bodega}} </option>
+                    @endforeach
+                </select>
 
             </div>
-            <div class="row">
-                <div class="mb-3 col-md-2">
-                    <label> TIPO DE DOCUMENTO</label>
-                    <select class="form-control"> 
-                        <option>FACTURA </option>
-                        <option>GUIA </option>
-                        <option>COMPROBANTE DE INGRESO </option>
-                    </select>
-                </div>
-                <div class="mb-3 col-md-2">
-                    <label> NRO DOCUMENTO </label>
-                    <input  class="form-control" type="text" name="">
-                </div>
+            <div class="mb-3 col-md-2">
+                <label> FECHA  </label>
+                <input class="form-control" name="fecha" type="text" size= "10"  id="fecha" value="<?php echo date("d/m/Y"); ?>" /> 
+
+            </div> 
+            <div class="mb-3 col-md-2">
+                <label> TIPO DE MOVIMIENTO  </label>
+                <input class="form-control "name="" type="text" value="INGRESO" disabled> 
+            </div>               
+
+
+            <div class="mb-3 col-md-2">
+                <label> ESTADO  </label>
+                <input class="form-control "name="" type="" value="DISPONIBLE" disabled > 
             </div>
-        </div>    
-    </div>
+
+        </div>
+
+    </div>    
+</div>
 </div>
 
 
@@ -60,30 +66,30 @@
     <div class="card-body">
 
         <form class="form-inline">  
-           <table class="table table-striped" style="width:100%">
-              <thead>
-                <tr>
-                  <th>Numero Detalle:</th>
-                  <th>Producto:</th>
-                  <th>Cantidad:</th>
-                  <th>Orden trabajo:</th>
-                  <th>Valor:</th>
-                  <th>Total:</th>
-              </tr>
-          </thead>
-          <tbody>
-            <tr ng-repeat="name in getdrugnameNewArray">
-                <td><input class="form-control" type="text" name="num_detalle" placeholder=""></td>
-                <td><input class="form-control" type="text" name="cod_producto" placeholder=""></td>
-                <td><input class="form-control" type="text" name="cantidad" placeholder=""></td>
-                <td><input class="form-control" type="text" name="orden_trabajo" placeholder=""></td>
-                <td><input class="form-control" type="text" name="valor" placeholder=""></td>
-                <td><input class="form-control" type="text" name="total" placeholder=""></td>
-                <td><button class="btn btn-primary" type="button" id="agregar_btn"  >Agregar  </button></td>
-            </tr>
-        </tbody>
-    </table>
-    <button type="button" class="btn btn-primary" id="guardar" > guardar </button>
+         <table class="table table-striped" style="width:100%">
+          <thead>
+            <tr>
+              <th>Numero Detalle:</th>
+              <th>Producto:</th>
+              <th>Cantidad:</th>
+              <th>Orden trabajo:</th>
+              <th>Valor:</th>
+              <th>Total:</th>
+          </tr>
+      </thead>
+      <tbody>
+        <tr ng-repeat="name in getdrugnameNewArray">
+            <td><input class="form-control" type="text" name="num_detalle" required placeholder=""></td>
+            <td><input class="form-control" type="text" name="cod_producto" required placeholder=""></td>
+            <td><input class="form-control" type="text" name="cantidad" required placeholder=""></td>
+            <td><input class="form-control" type="text" name="orden_trabajo" required  placeholder=""></td>
+            <td><input class="form-control" type="text" name="valor" required placeholder=""></td>
+            <td><input class="form-control" type="text" name="total" required placeholder=""></td>
+            <td><button class="btn btn-primary" type="button" id="agregar_btn"  >Agregar  </button></td>
+        </tr>
+    </tbody>
+</table>
+<input type="submit" class="btn btn-primary"  value="Enviar ">  </input>
 </form>
 
 
@@ -97,12 +103,12 @@
         $('#agregar_btn').on('click',function(){
             var html = '';
             html+='<tr>';
-            html+='<td><input class="form-control" type="text" name="num_detalle" placeholder=""></td>';
-            html+='<td><input class="form-control" type="text" name="cod_producto" placeholder=""></td>';
-            html+='<td><input class="form-control" type="text" name="cantidad" placeholder=""></td>';
-            html+='<td><input class="form-control" type="text" name="orden_trabajo" placeholder=""></td>';
-            html+='<td><input class="form-control" type="text" name="valor" placeholder=""></td>';
-            html+='<td><input class="form-control" type="text" name="total" placeholder=""></td>';
+            html+='<td><input class="form-control" type="text" name="num_detalle" required placeholder=""></td>';
+            html+='<td><input class="form-control" type="text" name="cod_producto" required placeholder=""></td>';
+            html+='<td><input class="form-control" type="text" name="cantidad" required placeholder=""></td>';
+            html+='<td><input class="form-control" type="text" name="orden_trabajo" required placeholder=""></td>';
+            html+='<td><input class="form-control" type="text" name="valor" required placeholder=""></td>';
+            html+='<td><input class="form-control" type="text" name="total" required placeholder=""></td>';
             html+='<td><button class="btn btn-primary" id="borrar_btn" type="button"> Eliminar </button></td>';
             html+='<tr>';       
             $('tbody').append(html);
